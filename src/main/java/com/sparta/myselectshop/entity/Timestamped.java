f2620131bb29532cce80a3d58c5605ef26b799e6
wogk9854 <wogk9843@naver.com>
2022-12-03 22:57:21 +0900

    Initial commit

---
+++ src/main/java/com/sparta/myselectshop/entity/Timestamped.java
@@ -0,0 +1,25 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt;
+}
